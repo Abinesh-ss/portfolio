15dda57a39d58f5f93631c920b5c9bffbdb67bc8
Abinesh-ss <119126115+Abinesh-ss@users.noreply.github.com>
2023-07-17 02:54:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+let menuIcon=document.querySelector('#menuicon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 let sections=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
+let navLinks=document.querySelectorAll('header nav a');
 
 window.onscroll=()=>{
     sections.forEach(sec=>{
         let top=window.scrollY;
-        let offset=sec.offsetTop-150;
+        let offset=sec.offsetTop - 150;
         let height=sec.offsetHeight;
         let id=sec.getAttribute('id');
 
         if(top >= offset && top<offset + height){
-            navlinks.forEach(links =>{
+            navLinks.forEach(links =>{
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+id+']').classList.add('active');
             });
@@ -17,4 +25,24 @@ window.onscroll=()=>{
     });
     let header=document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    
 };
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+ 
+ ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', {origin: 'bottom'});
+ ScrollReveal().reveal('.home-content h1, .about-img img', {origin: 'left'});
+ ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', {origin: 'right'});
+ 
+ 
+
